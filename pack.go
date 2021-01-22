e475c962a3ad72f62db1e1b0f1246bf70f5b974b
snmed <sandro@dallo.biz>
2021-01-22 17:30:55 +0100

    Add transitive dependency support

--- pack.go
+++ pack.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"errors"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
+	"unicode"
 
 	"github.com/go-sharp/color"
 )
 
 type PackCmd struct {
-	Module  []string `short:"m" long:"module" description:"Modules to pack (github.com/jessevdk/go-flags or github.com/jessevdk/go-flags@v1.4.0)"`
-	ModFile string   `short:"g" long:"go-mod-file" description:"Pack all dependencies specified in go.mod file."`
-	Output  string   `short:"o" long:"out" description:"Output file name of the zip archive." default:"gop_dependencies.zip"`
+	Module       []string `short:"m" long:"module" description:"Modules to pack (github.com/jessevdk/go-flags or github.com/jessevdk/go-flags@v1.4.0)"`
+	ModFile      string   `short:"g" long:"go-mod-file" description:"Pack all dependencies specified in go.mod file."`
+	Output       string   `short:"o" long:"out" description:"Output file name of the zip archive." default:"gop_dependencies.zip"`
+	DoTransitive bool     `short:"t" long:"transitive" description:"Ensure all transitive dependencies are included."`
 }
 
 // Execute will be called for the last active (sub)command. The
@@ -53,21 +57,20 @@ func (p *PackCmd) Execute(args []string) error {
 
 		for _, m := range p.Module {
 			verboseF("adding module: %v\n", color.BlueString(m))
-			cmd := exec.Command(commonOpts.GoBinPath, "get", m)
-			cmd.Dir = workDir
-			cmd.Env = append(os.Environ(), "GOMODCACHE="+modCache)
-			if output, err := cmd.CombinedOutput(); err != nil {
+			if output, err := getGoCommand(workDir, modCache, "get", m).CombinedOutput(); err != nil {
 				log.Printf("failed to add module: %v\n", color.RedString(m))
 				verboseF("%v: \n%s", color.RedString("error"), output)
 			}
 		}
+
+	}
+
+	if p.DoTransitive {
+		p.addTransitive(workDir, modCache)
 	}
 
 	log.Println("download all dependencies")
-	cmd := exec.Command(commonOpts.GoBinPath, "mod", "download", "all")
-	cmd.Dir = workDir
-	cmd.Env = append(os.Environ(), "GOMODCACHE="+modCache)
-	if err := cmd.Run(); err != nil {
+	if err := getGoCommand(workDir, modCache, "mod", "download", "all").Run(); err != nil {
 		log.Fatalln("failed to download dependencies:", color.RedString(err.Error()))
 	}
 
@@ -77,4 +80,76 @@ func (p *PackCmd) Execute(args []string) error {
 	}
 	log.Println("archive created:", color.GreenString(p.Output))
 	return nil
+}
+
+func (p *PackCmd) addTransitive(workDir, modCache string) {
+	hasMore := false
+
+	for {
+		output, err := getGoCommand(workDir, modCache, "mod", "graph").Output()
+		if err != nil {
+			log.Println("failed to add transitive dependencies:", color.RedString(err.Error()))
+			return
+		}
+
+		deps := strings.Split(string(output), "\n")
+		if len(deps) == 0 {
+			return
+		}
+
+		for _, dep := range deps {
+			mods := strings.Split(dep, " ")
+			mod := strings.Trim(mods[len(mods)-1], " ")
+
+			if mod == "" || folderExists(filepath.Join(modCache, moduleNameToCaseInsensitive(mod))) {
+				continue
+			}
+
+			verboseF("adding transitive module: %v\n", color.BlueString(mod))
+			if output, err := getGoCommand(workDir, modCache, "get", mod).CombinedOutput(); err != nil {
+				log.Printf("failed to add module: %v\n", color.RedString(mod))
+				verboseF("%v: \n%s", color.RedString("error"), output)
+			}
+			hasMore = true
+		}
+
+		if hasMore {
+			hasMore = false
+			continue
+		}
+		break
+	}
+
+}
+
+func getGoCommand(workDir, modCache string, args ...string) *exec.Cmd {
+	cmd := exec.Command(commonOpts.GoBinPath, args...)
+	cmd.Dir = workDir
+	cmd.Env = append(os.Environ(), "GOMODCACHE="+modCache)
+
+	return cmd
+}
+
+func folderExists(name string) bool {
+	if _, err := os.Stat(name); errors.Is(err, os.ErrNotExist) {
+		return false
+	}
+
+	return true
+}
+
+func moduleNameToCaseInsensitive(name string) string {
+	name = filepath.ToSlash(name)
+	var modName []rune
+
+	for _, v := range name {
+		if unicode.IsUpper(v) {
+			modName = append(modName, '!', unicode.ToLower(v))
+			continue
+		}
+
+		modName = append(modName, v)
+	}
+
+	return string(modName)
 }
