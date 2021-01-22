0729642cde5e7ec9315e60fd0ec3bd26a8b1ba55
snmed <sandro@dallo.biz>
2021-01-22 11:01:26 +0100

    Complete folder publisher

--- publish.go
+++ publish.go
@@ -3,10 +3,12 @@ package main
 import (
 	"errors"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 
 	"github.com/go-sharp/color"
 )
@@ -50,17 +52,26 @@ func (f FolderPublishCmd) Execute(args []string) error {
 
 	log.Println("processing files")
 	dirPrefix := filepath.Join(workDir, "cache", "download")
+	var wg sync.WaitGroup
 	err = filepath.Walk(dirPrefix, func(path string, info os.FileInfo, err error) error {
 		relPath := strings.TrimLeft(strings.TrimPrefix(path, dirPrefix), string(filepath.Separator))
 
 		if strings.HasPrefix(relPath, "sumdb") && !info.IsDir() {
-			go f.handleCopyFile(path, relPath)
+			wg.Add(1)
+			go func() {
+				f.handleCopyFile(path, relPath)
+				wg.Done()
+			}()
 			return nil
 		}
 
 		if info.IsDir() && strings.HasSuffix(relPath, "@v") {
-			go f.handleModule(path, relPath)
-			return nil
+			wg.Add(1)
+			go func() {
+				f.handleModule(path, dirPrefix)
+				wg.Done()
+			}()
+			return filepath.SkipDir
 		}
 
 		return nil
@@ -69,13 +80,66 @@ func (f FolderPublishCmd) Execute(args []string) error {
 	if err != nil {
 		log.Println(errorRedPrefix, err)
 	}
+	wg.Wait()
 
+	ppath, _ := filepath.Abs(f.Output)
+	log.Println("published archive to:", color.GreenString(ppath))
+	log.Printf("hint: set GOPROXY to use folder for dependencies:\n\t%v\n", color.BlueString("go env -w GOPROXY=file:///%v", ppath))
+	log.Printf("hint: in an air-gapped env set GOSUMDB to of:\n\t%v\n", color.BlueString("go env -w GOSUMDB=off"))
 	return nil
 }
 
-func (f FolderPublishCmd) handleModule(path, relPath string) {
-	//dstDir := filepath.Join(f.Output, relPath)
+func (f FolderPublishCmd) handleModule(path, prefix string) {
+	modD, err := os.Open(path)
+	if err != nil {
+		log.Println(errorRedPrefix, "failed to read module directory: ", err)
+		return
+	}
+	defer modD.Close()
 
+	files, err := modD.Readdirnames(0)
+	if err != nil {
+		log.Println(errorRedPrefix, "failed to read module directory: ", err)
+		return
+	}
+
+	// Copy files
+	for _, fi := range files {
+		if fi == "list" || fi == "list.lock" || fi == "lock" {
+			continue
+		}
+
+		srcF := filepath.Join(path, fi)
+		f.handleCopyFile(srcF, strings.TrimLeft(strings.TrimPrefix(srcF, prefix), string(filepath.Separator)))
+	}
+
+	var version []string
+	dstPath := filepath.Join(f.Output, strings.TrimLeft(strings.TrimPrefix(path, prefix), string(filepath.Separator)))
+	dstF, err := os.Open(dstPath)
+	if err != nil {
+		log.Println(errorRedPrefix, "failed to update list file: ", err)
+		return
+	}
+	defer dstF.Close()
+
+	modules, err := dstF.Readdirnames(0)
+	if err != nil {
+		log.Println(errorRedPrefix, "failed to update list file: ", err)
+		return
+	}
+
+	for _, v := range modules {
+		if strings.HasSuffix(v, ".mod") {
+			version = append(version, strings.TrimSuffix(v, ".mod"))
+		}
+	}
+
+	content := []byte(strings.Join(version, "\n"))
+	content = append(content, '\n')
+	if err := ioutil.WriteFile(filepath.Join(dstPath, "list"), content, 0664); err != nil {
+		log.Println(errorRedPrefix, "failed to update list file: ", err)
+		return
+	}
 }
 
 func (f FolderPublishCmd) handleCopyFile(path, relPath string) {
